5c61a68b22b58315add99a9cbee07e19d027eb3d
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-08-07 17:50:38 +0300

    add simple viewsets

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework import views, viewsets
 
-# Create your views here.
+from theatre.models import (
+    Actor,
+    Genre,
+    Play,
+    Performance,
+    Reservation,
+    TheatreHall,
+)
+from theatre.serializers import (
+    ActorSerializer,
+    GenreSerializer,
+    PlaySerializer,
+    PerformanceSerializer,
+    ReservationSerializer,
+    TheatreHallSerializer,
+)
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TheatreHallViewSet(viewsets.ModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.prefetch_related("genres", "actors")
+    serializer_class = PlaySerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.all().select_related("play", "theatre_hall")
+    serializer_class = PerformanceSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.prefetch_related(
+        "tickets__movie_session__movie", "tickets__movie_session__cinema_hall"
+    )
+    serializer_class = ReservationSerializer
+
