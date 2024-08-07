6bbce46d2534c172e3d6511381dc78b99b8dff69
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-08-07 16:12:48 +0300

    add projects files

---
+++ theatre_service/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
+    path("__debug__/", include("debug_toolbar.urls")),
+]
